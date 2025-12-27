f1cac6cde1641cda8de9881e645693d9dc6a5e85
Alex <kin_bravado.7h@icloud.com>
2025-12-27 12:07:15 +0000

    Dynamic favicon set via HTML rather than JS

--- home/scripts-home.js
+++ home/scripts-home.js
@@ -12,29 +12,29 @@ if (!re.test(userAgent)) {
 // Changes favicon based on system theme
 // May not work in Safari due to aggressive caching
 
-function themedFavicon(isDark) {
- var favicons = document.querySelectorAll('.dynamic-favicon');
- favicons.forEach(favicon => {
-     if (isDark) {
-         favicon.href = favicon.href.replace(/\/light\//, '/dark/');
-     } else {
-         favicon.href = favicon.href.replace(/\/dark\//, '/light/');
-     }
- });
-}
+// function themedFavicon(isDark) {
+//  var favicons = document.querySelectorAll('.dynamic-favicon');
+//  favicons.forEach(favicon => {
+//      if (isDark) {
+//          favicon.href = favicon.href.replace(/\/light\//, '/dark/');
+//      } else {
+//          favicon.href = favicon.href.replace(/\/dark\//, '/light/');
+//      }
+//  });
+// }
 
-// Set themed favicon on page load
-const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches;
-if (isLightMode && !isBot) {
-    themedFavicon(false);  // Use light mode
-} else {
-    themedFavicon(true);   // Use dark mode
-}
+// // Set themed favicon on page load
+// const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches;
+// if (isLightMode && !isBot) {
+//     themedFavicon(false);  // Use light mode
+// } else {
+//     themedFavicon(true);   // Use dark mode
+// }
 
-// Set themed favicon on theme change
-window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', e => {
-    themedFavicon(!e.matches);
-});
+// // Set themed favicon on theme change
+// window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', e => {
+//     themedFavicon(!e.matches);
+// });
 
 // Terminal navigation
 document.querySelectorAll('.terminal-line[data-scroll-to]').forEach(line => {
